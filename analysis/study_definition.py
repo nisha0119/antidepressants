b9cc8f2907d68be96d4ba3a4fd95bf43b91edcc0
nisha0119 <nish.rajendran@gmail.com>
2021-09-01 09:22:55 +0000

    Draft with codes imported

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -45,5 +45,74 @@ study = StudyDefinition(
             return_expectations={"earliest": "2019-03-01", "latest": "2020-03-22"},
             returning = "binary_flag"
             
-    )
+    ),
+
+    
+   ADpostcovid = patients.with_these_medications(
+            antidepressantsall_codes,
+            between ["2020-03-23", "2021-03-31"],
+            return_last_date_in_period=TRUE, # to obtain latest date on AD preCOVID
+            return_expectations={"earliest": "2020-03-23", "latest": "2021-03-31"},
+            returning = "binary_flag"
+
+   ),
+
+    ageprecovid=patients.age_as_of(
+            "2020-03-01",
+            return_expectations={
+                "rate": "universal",
+                "int": {"distribution":"population_ages"},
+        }
+    ),
+
+    agepostcovid=patients.age_as_of(
+        "2021-03-01",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution":"population_ages"},
+        }
+    ),
+
+    #patients.sex,#( 
+    #return_expectations={
+     #   "rate": "universal",
+     #   "category": {"ratios": {"M": 0.49, "F": 0.51}
+     #   }
+    #),
+
+    ethnicity_by_16_grouping=patients.with_ethnicity_from_sus(
+    returning="group_16",
+    use_most_frequent_code=True,
+    ),
+
+    imd=patients.address_as_of(
+    "2020-02-29",
+    returning="index_of_multiple_deprivation",
+    round_to_nearest=100,
+    return_expectations={
+        "rate": "universal",
+        "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
+    },
+    ),
+
+    region=patients.registered_practice_as_of(
+    "2020-02-01",
+    returning="nuts1_region_name",
+    return_expectations={
+        "rate": "universal",
+        "category": {
+            "ratios": {
+                "North East": 0.1,
+                "North West": 0.1,
+                "Yorkshire and the Humber": 0.1,
+                "East Midlands": 0.1,
+                "West Midlands": 0.1,
+                "East of England": 0.1,
+                "London": 0.2,
+                "South East": 0.2,
+            },
+        },
+    },
+    ),
+
 )
